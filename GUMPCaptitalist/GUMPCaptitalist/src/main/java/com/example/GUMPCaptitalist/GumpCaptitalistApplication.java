ec4919215dfda0cffae3fb59735e1fc0df661b4b
JulesISIS <60700420+JulesISIS@users.noreply.github.com>
2022-02-01 09:45:37 +0100

    Création du projet

---
+++ GUMPCaptitalist/GUMPCaptitalist/src/main/java/com/example/GUMPCaptitalist/GumpCaptitalistApplication.java
@@ -0,0 +1,13 @@
+package com.example.GUMPCaptitalist;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GumpCaptitalistApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GumpCaptitalistApplication.class, args);
+	}
+
+}
